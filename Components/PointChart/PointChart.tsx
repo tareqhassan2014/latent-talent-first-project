5d4a48ffb1ad414bc39ca9aa832c3ec8f913a392
ibrahimk4111 <ibrahimk4111@gmail.com>
2024-03-15 16:59:36 +0600

    fixs

--- Components/PointChart/PointChart.tsx
+++ Components/PointChart/PointChart.tsx
@@ -2,15 +2,14 @@ import { BarChart } from "../BarChart/BarChart";
 import LineChart from "../LineChart/LineChart";
 
 export function PointChart() {
- 
-
   return (
     <div className=" max-w-[1280px] mx-auto mt-10 grid grid-cols-12 gap-10">
-      {/* line point chart */}
+      {/* line chart */}
       <section className=" col-span-7 ">
         <LineChart />
       </section>
 
+      {/* Column Bar chart */}
       <section className=" col-span-5 ">
         <BarChart />
       </section>
